4d735b6c0125a2446ceda9c8497799317c9792ea
ibhaudur rahman <ibhaudurrahman1999@gmail.com>
2024-11-04 10:16:09 +0530

    scroll animation

--- components/NavHeader.tsx
+++ components/NavHeader.tsx
@@ -4,6 +4,7 @@ import { usePathname } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 import { RxHamburgerMenu } from "react-icons/rx";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 const Menu = [
   { name: "About", path: "about" },
@@ -17,6 +18,12 @@ const NavHeader = () => {
   const [active, setActive] = useState("");
   const pathName = usePathname();
   let pathValidate = pathName?.split("/")?.includes("admin");
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
   useEffect(() => {
     const changeBackground = () => {
       if (window.scrollY >= 30) {
@@ -33,58 +40,64 @@ const NavHeader = () => {
     };
   }, []);
   return (
-    <header
-      className={`Header fixed top-0 left-0 right-0 z-10 ${
-        navbar ? "bg-gray-700" : "bg-gray-800"
-      }`}
-    >
-      <nav>
-        <Container className="mx-auto max-w-7xl px-2 sm:px-6 sm:py-3 lg:px-8 ">
-          <div className="relative flex h-16 items-center justify-between px-2 sm:px-0">
-            <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
-              <div className="flex flex-shrink-0 items-center">
-                {/* <img
+    <>
+      <header
+        className={`Header fixed top-0 left-0 right-0 z-10 ${
+          navbar ? "bg-gray-700" : "bg-gray-800"
+        }`}
+      >
+        {" "}
+        <nav>
+          <Container className="mx-auto max-w-7xl px-2 sm:px-6 sm:py-3 lg:px-8 ">
+            <div className="relative flex h-16 items-center justify-between px-2 sm:px-0">
+              <div className="flex flex-1 items-center sm:items-stretch sm:justify-start">
+                <div className="flex flex-shrink-0 items-center">
+                  {/* <img
                   className="h-8 w-auto"
                   src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                   alt="Your Company"
                 /> */}
-                <h3 className="text-orange-500">Portfolio</h3>
+                  <h3 className="text-orange-500">Portfolio</h3>
+                </div>
               </div>
-            </div>
-            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-              <RxHamburgerMenu className="sm:hidden text-2xl text-green-500" />
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {pathValidate ? (
-                    <Link href="/" className="inactive" aria-current="page">
-                      Portfolio
-                    </Link>
-                  ) : (
-                    Menu.map((item, index) => {
-                      return (
-                        <Link
-                          href={
-                            item.name === "Admin" ? item.path : `#${item.path}`
-                          }
-                          key={index}
-                          onClick={() => setActive(item.path)}
-                          className={
-                            active === item.path ? "active" : "inactive"
-                          }
-                          aria-current="page"
-                        >
-                          {item.name}
-                        </Link>
-                      );
-                    })
-                  )}
+              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <RxHamburgerMenu className="sm:hidden text-2xl text-green-500" />
+                <div className="hidden sm:ml-6 sm:block">
+                  <div className="flex space-x-4">
+                    {pathValidate ? (
+                      <Link href="/" className="inactive" aria-current="page">
+                        Portfolio
+                      </Link>
+                    ) : (
+                      Menu.map((item, index) => {
+                        return (
+                          <Link
+                            href={
+                              item.name === "Admin"
+                                ? item.path
+                                : `#${item.path}`
+                            }
+                            key={index}
+                            onClick={() => setActive(item.path)}
+                            className={
+                              active === item.path ? "active" : "inactive"
+                            }
+                            aria-current="page"
+                          >
+                            {item.name}
+                          </Link>
+                        );
+                      })
+                    )}
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        </Container>
-      </nav>
-    </header>
+          </Container>
+        </nav>
+      </header>
+      <motion.div className="progress-bar" style={{ scaleX }} />
+    </>
   );
 };
 
